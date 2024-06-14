79d92ed5e2528979ac4314286fc24911aa6e2f5b
Amanda Davis <amanda.davis625@gmail.com>
2024-06-13 22:15:42 -0700

    Trying to Fix GitHub Errors

--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -2,33 +2,33 @@
 const config = require('../config');
 
 test('Request status code should be 200', async () => {
-	let actualResponseCode;
+    let actualResponseCode;
     try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits/2:id`, {
-			method: 'DELETE',
-		});
-		actualResponseCode = response.status;
-	} catch (error) {
-		console.error(error);
-	}
+        const response = await fetch(`${config.API_URL}/api/v1/kits/2:id`, {
+            method: 'DELETE',
+        });
+        actualResponseCode = response.status;
+    } catch (error) {
+        console.error(error);
+    }
 
-	expect(actualResponseCode).toBe(200);
+    expect(actualResponseCode).toBe(200);
 });
 
-test('Request body should contain "ok": true', async () => {
-	let actualResponseCode;
+test('Response body should contain "ok": true', async () => {
+    let actualResponseBody;
     try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits/2:id`, {
-			method: 'DELETE',
-		});
-		actualResponseBody = await response.json();
-	} catch (error) {
-		console.error(error);
-	}
+        const response = await fetch(`${config.API_URL}/api/v1/kits/2:id`, {
+            method: 'DELETE',
+        });
+        actualResponseBody = await response.json();
+    } catch (error) {
+        console.error(error);
+    }
 
-	const expectedSuccessResponse = {
+    const expectedSuccessResponse = {
         "ok": true,
-	}
+    };
 
-	expect(actualResponseBody).toMatchObject(expectedResponse);
+    expect(actualResponseBody).toMatchObject(expectedSuccessResponse);
 });
