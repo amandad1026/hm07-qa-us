68d6a32a486a35230bf13bc17015cd6753dfa639
Amanda Davis <amanda.davis625@gmail.com>
2024-06-13 22:11:10 -0700

    Fixing GitHub Errors

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-d25abe96-59b2-4cef-8317-ce617a79b0df.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-9197c6a7-f168-4c64-bd50-f62a1791c24c.containerhub.tripleten-services.com"
 }
--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -15,10 +15,10 @@ test('Request status code should be 200', async () => {
 	expect(actualResponseCode).toBe(200);
 });
 
-test('Request body should contain ok:', async () => {
+test('Request body should contain "ok": true', async () => {
 	let actualResponseCode;
     try {
-		response = await fetch(`${config.API_URL}/api/v1/kits/2:id`, {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/2:id`, {
 			method: 'DELETE',
 		});
 		actualResponseBody = await response.json();
@@ -26,9 +26,9 @@ test('Request body should contain ok:', async () => {
 		console.error(error);
 	}
 
-	const expectedResponse = {
-        ok: true,
-	};
+	const expectedSuccessResponse = {
+        "ok": true,
+	}
 
 	expect(actualResponseBody).toMatchObject(expectedResponse);
 });
